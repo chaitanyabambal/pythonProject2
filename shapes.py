42f434507708bf96e0bf7a9269dfd7309dd65c1a
chaitanyabambal <chaitanyabambal@gmail.com>
2022-03-28 15:45:02 +0530

    cal

---
+++ shapes.py
@@ -0,0 +1,27 @@
+def rectangle(a, b):
+    return a*b
+def perimeter(a, b):
+       return a+a+b+b
+def square(a):
+    return  a*a
+
+
+
+if __name__=="__main__":
+    a1=int(input("Enter value1"))
+    a2=int(input("Enter value2"))
+
+
+
+
+print("Menu\n 1.Area of Rectangle\n 2.Perimeter of Reactangle\n 3.Area of Square")
+x = int(input("Please Enter your Choice Given:\n"))
+if x==1:
+    print( rectangle(a1,a2))
+elif x==2:
+    print(perimeter(a1, a2))
+
+elif x==3:
+    print(square(a1))
+else:
+    print("Its the wrong input")
