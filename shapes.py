b09d283e42fb1b570cb3beedde2053a458c47414
chaitanyabambal <chaitanyabambal@gmail.com>
2022-03-31 12:13:36 +0530

    28-03

--- shapes.py
+++ shapes.py
@@ -9,7 +9,7 @@ def square(a):
 
 if __name__=="__main__":
 
-print("Menu\n 1.Area of Rectangle\n 2.Perimeter of Reactangle\n 3.Area of Square")
+   print('''Menu\n 1.Area of Rectangle\n 2.Perimeter of Reactangle\n 3.Area of Square''')
 x = int(input("Please Enter your Choice Given:\n"))
 if x==1:
     a1 = int(input("Enter value1"))
--- test_menudriven.py
+++ test_menudriven.py
@@ -1,7 +1,19 @@
 import shapes
 
 
-def test_area():
-    x=20
-    y=30
-    result= shapes.rectangle()
+def test_areaRec():
+    x=5
+    y=7
+    result=shapes.rectangle(x,y)
+    assert x*y==result
+
+def test_periRec():
+    x=5
+    y=8
+    result=shapes.perimeter(x,y)
+    assert x+x+y+y==result
+
+def test_areaSquare():
+    x=8
+    result=shapes.square(x)
+    assert x*x==result
